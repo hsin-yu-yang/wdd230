b44abb0e50a903dee9d037ec0ef3f0b22d13b6d9
hsin-yu-yang <yan20012@byui.edu>
2023-10-12 15:06:02 -0600

    correction

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,29 @@ const currentYearOptions = {
 }
 lastModified.textContent = ` Last Modified: ${new Date(document.lastModified).toLocaleDateString("en-US", lastModifiedOptions)}`;
 currentYear.textContent = `${new Date().toLocaleDateString("en-US", currentYearOptions)}`
+
+/* HAMBURGER BUTTON */
+function toggleMenu() {
+    let nav=document.getElementById("primaryNav")
+    let hamburgerBtn=document.getElementById("hamburgerBtn")
+    console.log(nav)
+    if (nav.style.display=== "block"){
+        nav.style.display="none";
+        hamburgerBtn.src="chamber/images/hamburger.png"  
+    } else{
+        nav.style.display= "block";
+        hamburgerBtn.src="chamber/images/x.png"
+    }
+    };
+    
+    const x = document.getElementById("hamburgerBtn")
+    x.addEventListener("click",toggleMenu);
+    
+    function toggledarkmode(){
+        var body = document.body
+        body.classList.toggle("dark_mode")
+        var header = document.header
+        header.classList.toggle("dark_mode")
+    }
+    const buttton = document.getElementById("dark_mode")
+    buttton.addEventListener("click",toggledarkmode);
